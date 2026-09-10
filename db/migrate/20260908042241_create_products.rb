b06c177d777f437cdad82840a363d29e1054951f
Dylan Nguyen <ahuyadug@gmail.com>
2026-09-10 17:30:32 +0700

    Fix rubocop formatting

--- db/migrate/20260908042241_create_products.rb
+++ db/migrate/20260908042241_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[8.1]
   def change
     create_table :products do |t|
       t.string :title, null: false
-      t.text :description 
+      t.text :description
       t.string :image_url
       t.decimal :price
       t.bigint :category_id
