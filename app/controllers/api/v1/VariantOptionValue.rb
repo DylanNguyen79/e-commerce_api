bd8fccec1a88e6dcd86ad060ff8a9eedb6f6bd6e
Dylan Nguyen <ahuyadug@gmail.com>
2026-09-14 17:34:33 +0700

    Still Get ID for VariantOptionValue

--- app/controllers/api/v1/VariantOptionValue.rb
+++ app/controllers/api/v1/VariantOptionValue.rb
@@ -15,3 +15,86 @@ VariantOptionValue.create(option_id: 2, option_value_id: 4, variant_id: 5)
 
 VariantOptionValue.create(option_id: 1, option_value_id: 2, variant_id: 6)
 VariantOptionValue.create(option_id: 2, option_value_id: 5, variant_id: 6)
+
+variant = [ 1, 2, 3, 4, 5, 6 ]
+option = [ 1, 2, 3]
+option_values = [ [ 1, 2 ], [ 3, 4, 5 ], [6]]
+
+variant.loop do |var, i|
+  variant.size / optione_values[i].size
+  option.loop do  |opt, i|
+    0 - 0 - [0][0]      1
+    0 - 1 - [1][0]
+    0 - 2 - [2][0]
+
+    1 - 0 - [0][0]      2
+    1 - 1 - [1][1]
+    1 - 2 - [2][0]
+
+    2 - 0 - [0][0]      3
+    2 - 1 - [1][2]    
+    2 - 2 - [2][0]
+
+    3 - 0 - [0][1]      4 
+    3 - 1 - [1][0]    
+    3 - 2 - [2][0]
+
+    4 - 0 - [0][1]      5
+    4 - 1 - [1][1]    
+    4 - 2 - [2][0]
+
+    5 - 0 - [0][1]      6
+    5 - 1 - [1][2]
+    5 - 2 - [2][0]
+
+    j = i
+    opt_size = option[i].size
+    k += ?? / ?? 
+     VariantOptionValue.create(variant_id: var, option_id: opt, option_value_id: option_values[i][k])
+ end
+end
+
+option_values[i].size
+option_values[0].size = 2
+option_values[1].size = 3
+repeat = var.size / option_values[i].size
+
+vov_count = variant.size * option.size = 12 
+
+0                     0, 0, 0, 1, 1, 1
+opt_id: 1, opt_value: 1, 1, 1, 2, 2, 2
+
+1                     2  3  4  2  3  4
+opt_id: 2, opt_value: 3, 4, 5, 3, 4, 5
+
+2                     3  3  3  3  3  3
+opt_id: 3, opt_value: 6, 6, 6, 6, 6, 6
+
+
+expected: 
+[variant: 1, option: 1, option_values: 1]
+[variant: 1, option: 2, option_values: 3]
+[variant: 1, option: 3, option_values: 6]
+
+[variant: 2, option: 1, option_values: 1]
+[variant: 2, option: 2, option_values: 4]
+[variant: 2, option: 3, option_values: 6]
+
+
+[variant: 3, option: 1, option_values: 1]
+[variant: 3, option: 2, option_values: 5]
+[variant: 3, option: 3, option_values: 6]
+
+
+[variant: 4, option: 1, option_values: 2]
+[variant: 4, option: 2, option_values: 3]
+[variant: 4, option: 3, option_values: 6]
+
+[variant: 5, option: 1, option_values: 2]
+[variant: 5, option: 2, option_values: 4]
+[variant: 5, option: 3, option_values: 6]
+
+[variant: 6, option: 1, option_values: 2]
+[variant: 6, option: 2, option_values: 5]
+[variant: 6, option: 3, option_values: 6]
+
--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -5,24 +5,23 @@ class Api::V1::ProductsController < ApplicationController
     puts ("new")
     puts product.inspect
     variant_count = 1
-    vov_count = 1
     variant_ids = []
     option_ids = []
     option_value_ids = []
     option_value_group = []
     params[:options].map do |option|
-        opt = Option.create(name: option["name"])
+        opt = Option.create!(name: option["name"])
         option_ids.push(opt.id)
+
         variant_count *= option[:option_values].size
+
         option_value_ids = []
         option[:option_values].map do |opt_val|
-            option_value = OptionValue.create(values: opt_val, option_id: opt.id)
+            option_value = OptionValue.create!(values: opt_val, option_id: opt.id)
             option_value_ids.push(option_value.id)
         end
         option_value_group.push(option_value_ids)
     end
-
-    vov_count = variant_count * params[:options].size
 
     variant_count.times do |i|
         variant = Variant.create(product_id: product.id)
@@ -35,7 +34,31 @@ class Api::V1::ProductsController < ApplicationController
     puts ("option_value_ids")
     puts (option_value_ids)
     puts ("option_value_group")
-    pp (option_value_group)
+    puts (option_value_group)
+    puts ("option_value_ids.size")
+    puts (option_value_ids.size)
+    puts ("option_value_group.flatten")
+    puts (option_value_group.flatten)
+
+    combination = option_value_group[0].product(option_value_group[1])
+
+    combination.each_with_index do |option_values, index|
+      variant_id = variant_ids[index]
+
+      option_values.each_with_index do |option_value_id, option_index|
+        option_id = option_ids[index]
+
+        puts ("option_id")
+        puts (option_id)
+        variant_option_value = VariantOptionValue.create!(
+            variant_id: variant_id,
+            option_id: option_id,
+            option_value_id: option_value_id
+        )
+
+        puts (variant_option_value.inspect)
+      end
+    end
 
     render plain: "ok"
   end
